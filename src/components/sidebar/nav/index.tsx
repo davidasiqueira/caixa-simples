c1baec8a58df0b7ee1e0a5bfeeacb369397c10df
David Siqueira <davidsiqueira.hog@gmail.com>
2023-02-05 15:40:56 -0300

    ajustes sidebar

--- src/components/sidebar/nav/index.tsx
+++ src/components/sidebar/nav/index.tsx
@@ -1,11 +1,20 @@
-import styles from './nav.module.scss'
-import { ChartLogo, StoreLogo } from '@/components/svgs'
+import styles from "./nav.module.scss";
+import { ChartLogo, StoreLogo } from "@/components/svgs";
+import Link from "next/link";
 
 export const Nav = () => {
   return (
     <div className={styles.nav}>
-      <ChartLogo />
-      <StoreLogo /> 
+      <Link href="/dashbord">
+        <a>
+          <ChartLogo />
+        </a>
+      </Link>
+      <Link href="/lancamentos">
+        <a>
+          <StoreLogo />
+        </a>
+      </Link>
     </div>
-  )
-}
+  );
+};
